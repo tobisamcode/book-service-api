c748f6bf8420279ca540b188fc9ceae30c5f756e
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-19 15:33:22 +0100

    testing app and configuration

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,1 +1,18 @@
 import { Sequelize } from "sequelize";
+
+export default class Database {
+  private sequelize: Sequelize;
+  constructor() {
+    this.sequelize = new Sequelize({
+      dialect: "mysql",
+      host: "localhost",
+      username: "root",
+      password: "Nelson2000@@@",
+      database: "books"
+    });
+  }
+
+  getSequelizeConnection(): Sequelize {
+    return this.sequelize;
+  }
+}
