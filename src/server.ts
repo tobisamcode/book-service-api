ac8e9fad0c7283838c5d8a47a5b540327fcea2d6
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-19 15:33:15 +0100

    testing app and configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express, { Application } from "express";
 import { Request, Response } from "express";
+import bookRouter from "./routes/book-routes";
 
 // creates the express application
 
 const app: Application = express();
 
 const PORT: number | undefined = Number(process.env.PORT) || 3000;
+
+app.use("/books", bookRouter);
 
 // Creating a book
 // Read all books from datastore
