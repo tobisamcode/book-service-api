c8549b40989b921c0ee13fd97de5e903d4968500
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-19 01:10:22 +0100

    testing app and configuration

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("up and running");
+import express, { Application } from "express";
+import { Request, Response } from "express";
+
+// creates the express application
+
+const app: Application = express();
+
+const PORT: number | undefined = Number(process.env.PORT) || 3000;
+
+// Creating a book
+// Read all books from datastore
+// Read a book
+// Update a book
+// Delete a book
+
+app.listen(PORT, () => {
+  console.log(`Application running on port ${PORT}`);
+});
